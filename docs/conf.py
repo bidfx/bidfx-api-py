1f417b8237a17a3d810e29698d43a3d0e0f4f8b9
Paul Sweeny <paul.sweeny@tradingscreen.com>
2020-03-10 18:41:59 +0000

    comment out latext as it creates error on rtfd

--- docs/conf.py
+++ docs/conf.py
@@ -59,20 +59,3 @@ autoclass_content = "both"
 # PDF -----------------------------------------------------------------
 
 latex_logo = "_static/bidfx_logo_128.png"
-
-# latex_elements = {
-#     "papersize": "a4paper",
-#     "pointsize": "12pt",
-#     "preamble": "",
-#     "figure_align": "htbp",
-# }
-# latex_paper_size = "a4paper"
-# latex_documents = [
-#     (
-#         "index",
-#         u"bidfx-public-api-python.pdf",
-#         u"BidFX Public API for Python",
-#         u"Paul Sweeny",
-#         u"manual",
-#     ),
-# ]
